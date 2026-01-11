6fd339c9426de3d091a73f0d30a075d6e67adf4e
Humayra Afrin Chowdhury <44744453+HumayraAfrin@users.noreply.github.com>
2026-01-11 23:55:00 +0600

    Create Project Employees I.sql

---
+++ scripts/database/easy/1075. Project Employees I/Project Employees I.sql
@@ -0,0 +1,5 @@
+select p.project_id, round(avg(cast(e.experience_years as decimal(10,2))), 2) as average_years
+from Project p
+inner join Employee e on p.employee_id = e.employee_id
+group by p.project_id
+order by 1
